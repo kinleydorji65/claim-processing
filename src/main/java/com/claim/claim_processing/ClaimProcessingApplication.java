b084d602df77d92b3c885c3a473d6d6e68a55cab
kinleydorji65 <kinley5kinley5@gmail.com>
2026-04-10 10:39:17 +0600

    Initial commit - claim processing project setup

---
+++ src/main/java/com/claim/claim_processing/ClaimProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.claim.claim_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claim/claim_processing/ClaimProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claim.claim_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
